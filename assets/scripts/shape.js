b4dbf4ac2db91ea57217b54e236448931369affb
Alex Fur <alexfur.art.comms@gmail.com>
2021-12-16 14:36:59 -0600

    Add shape complexity

--- assets/scripts/shape.js
+++ assets/scripts/shape.js
@@ -7,6 +7,8 @@ function newPoint(canvas) {
     var y = getRandomInt(canvas.height);
     return {x: x, y: y}
 }
+
+var drawFunctions = [bezier, quad, arc, line];
   
 function drawRandomShape(ctx, vertCount, color) {
     var p = newPoint(canvas);
@@ -19,7 +21,7 @@ function drawRandomShape(ctx, vertCount, color) {
         var p2 = newPoint(canvas);
         var p3 = newPoint(canvas);
 
-        ctx.bezierCurveTo(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y);
+        drawFunctions[getRandomInt(drawFunctions.length)](ctx, p1, p2, p3);
     }
 
     ctx.moveTo(p.x, p.y);
@@ -29,6 +31,30 @@ function drawRandomShape(ctx, vertCount, color) {
     ctx.stroke();
     ctx.fill();
     ctx.closePath();
+}
+
+function bezier(ctx, p1, p2, p3) {
+    ctx.bezierCurveTo(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y);
+}
+
+function quad(ctx, p1, p2, p3) {
+    var point1 = choosePoint(p1, p2, p3);
+    var point2 = choosePoint(p1, p2, p3);
+    ctx.quadraticCurveTo(point1.x, point1.y, point2.x, point2.y);
+}
+
+function arc(ctx, p1, p2, p3) {
+    ctx.arcTo(p1.x, p1.y, p2.x, p2.y, p3.x / p3.y);
+}
+
+function line(ctx, p1, p2, p3) {
+    var point = choosePoint(p1, p2, p3);
+    ctx.lineTo(point.x, point.y);
+}
+
+function choosePoint(p1, p2, p3) {
+    var points = [p1, p2, p3];
+    return points[getRandomInt(points.length)];
 }
   
 function drawShape(canvas, shapesNumber, vertextNumber, color) {
